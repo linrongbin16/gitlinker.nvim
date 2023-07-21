8c97dca7bf778195074b3fd1cf8248e13c9eaad7
linrongbin16 <linrongbin16@outlook.com>
2023-07-22 06:10:32 +0800

    fix: windows symlink

--- lua/gitlinker/util.lua
+++ lua/gitlinker/util.lua
@@ -23,6 +23,9 @@ local function relative_path(cwd)
   if cwd == nil or string.len(cwd) <= 0 then
     cwd = vim.fn.getcwd()
   end
+  -- get real path from possibly symlink
+  cwd = vim.fn.resolve(cwd)
+  -- normalize path slash from '\\' to '/'
   if cwd:find("\\") then
     cwd = cwd:gsub("\\\\", "/")
     cwd = cwd:gsub("\\", "/")
