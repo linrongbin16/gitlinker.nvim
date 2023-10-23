f1ee7e711c587db25a24eeb12505eb3bdd47a988
linrongbin16 <linrongbin16@outlook.com>
2023-10-23 12:42:12 +0800

    test(git): add cases

--- test/git_spec.lua
+++ test/git_spec.lua
@@ -7,6 +7,10 @@ describe("git", function()
 
     before_each(function()
         vim.api.nvim_command("cd " .. cwd)
+        vim.opt.swapfile = false
+        local logger = require("gitlinker.logger")
+        logger.setup()
+        vim.cmd([[ edit lua/gitlinker.lua ]])
     end)
 
     local logger = require("gitlinker.logger")
@@ -16,6 +20,7 @@ describe("git", function()
         file_log = true,
     })
     local git = require("gitlinker.git")
+    local path = require("gitlinker.path")
     describe("[git]", function()
         it("_get_remote", function()
             local r = git._get_remote()
@@ -54,6 +59,39 @@ describe("git", function()
                 assert_true(string.len(rev) > 0)
             else
                 assert_true(rev == nil)
+            end
+        end)
+        it("is_file_in_rev", function()
+            local remote = git.get_branch_remote()
+            if not remote then
+                assert_true(remote == nil)
+                return
+            end
+            assert_eq(type(remote), "string")
+            assert_true(string.len(remote) > 0)
+            local remote_url = git.get_remote_url(remote)
+            if not remote_url then
+                assert_true(remote_url == nil)
+                return
+            end
+            assert_eq(type(remote_url), "string")
+            assert_true(string.len(remote_url) > 0)
+
+            local rev = git.get_closest_remote_compatible_rev(remote) --[[@as string]]
+            if not rev then
+                assert_true(rev == nil)
+                return
+            end
+            local bufpath = path.buffer_relpath() --[[@as string]]
+            if not bufpath then
+                assert_true(bufpath == nil)
+                return
+            end
+            local actual = git.is_file_in_rev(bufpath, rev)
+            if actual ~= nil then
+                print(string.format("is_file_in_rev:%s\n", vim.inspect(actual)))
+            else
+                assert_true(actual == nil)
             end
         end)
     end)
