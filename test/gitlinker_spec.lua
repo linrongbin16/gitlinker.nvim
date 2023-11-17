58b6f2347d4f924271b3fec6319f0c1537a41d2a
linrongbin16 <linrongbin16@outlook.com>
2023-11-17 10:14:28 +0800

    chore

--- test/gitlinker_spec.lua
+++ test/gitlinker_spec.lua
@@ -5,35 +5,16 @@ describe("gitlinker", function()
   local assert_true = assert.is_true
   local assert_false = assert.is_false
 
+  local gitlinker = require("gitlinker")
+
   before_each(function()
     vim.api.nvim_command("cd " .. cwd)
     vim.opt.swapfile = false
-    local logger = require("gitlinker.logger")
-    logger.setup()
+    gitlinker.setup()
     vim.cmd([[ edit lua/gitlinker.lua ]])
   end)
 
-  local gitlinker = require("gitlinker")
   local utils = require("gitlinker.utils")
-  describe("[link]", function()
-    it("link", function()
-      gitlinker.setup()
-      local actual = gitlinker.link({
-        action = require("gitlinker.actions").clipboard,
-        router = require("gitlinker.routers").blob,
-      })
-      print(string.format("link:%s\n", vim.inspect(actual)))
-      if actual then
-        assert_eq(type(actual), "string")
-        assert_true(
-          utils.string_startswith(
-            actual,
-            "https://github.com/linrongbin16/gitlinker.nvim/blob"
-          )
-        )
-      end
-    end)
-  end)
   describe("_browse/_blame", function()
     it("without line numbers", function()
       local actual = gitlinker._browse({
