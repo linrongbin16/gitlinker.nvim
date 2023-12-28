6ab1117e2e91f7e3e20dbf5e0c51be4a20307e36
linrongbin16 <linrongbin16@outlook.com>
2023-12-28 16:44:15 +0800

    chore

--- test/git_spec.lua
+++ test/git_spec.lua
@@ -11,17 +11,10 @@ describe("git", function()
     vim.cmd([[ edit lua/gitlinker.lua ]])
   end)
 
-  local LogLevels = require("gitlinker.commons.logger").LogLevels
-  local logger = require("gitlinker.commons.logger")
-  logger.setup({
-    name = "[gitlinker]",
-    level = LogLevels.DEBUG,
-    console_log = true,
-    file_log = true,
-    file_log_name = "gitlinker.log",
-  })
   local git = require("gitlinker.git")
   local path = require("gitlinker.path")
+  local gitlinker = require("gitlinker")
+  pcall(gitlinker.setup, {})
   describe("[git]", function()
     it("_get_remote", function()
       local r = git._get_remote()
