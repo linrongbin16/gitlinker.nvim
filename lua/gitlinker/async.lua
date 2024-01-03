5d1d8084750341189e4b32bb8a428faf621b9fa6
linrongbin16 <linrongbin16@outlook.com>
2024-01-03 16:57:25 +0800

    chore

--- lua/gitlinker/async.lua
+++ lua/gitlinker/async.lua
@@ -153,7 +153,7 @@ function M.wait(...)
     return wait(...)
   end
 
-  -- Assume argc is equal to the number of passed arguments.
+  -- Asume argc is equal to the number of passed arguments.
   return wait(select("#", ...) - 1, ...)
 end
 
