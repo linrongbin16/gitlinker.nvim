978e915601eeea448d568c4720360af02d69e8e8
linrongbin16 <linrongbin16@outlook.com>
2024-01-08 21:38:06 +0800

    Update gitlinker_spec.lua

--- test/gitlinker_spec.lua
+++ test/gitlinker_spec.lua
@@ -771,13 +771,25 @@ describe("gitlinker", function()
   end)
 
     describe ('[void_link]', function ()
-      it('test', function () 
+      it('link browse', function () 
           gitlinker.void_link({
                 action = require('gitlinker.actions').clipboard,
                 router = function(lk)
         return require('gitlinker')._router('browse', lk)
-      end,lstart = 1,
+      end,
+                lstart = 1,
       lend = 1,
+              })
+          end)
+        it('link blame', function () 
+          gitlinker.void_link({
+                action = require('gitlinker.actions').clipboard,
+                router = function(lk)
+        return require('gitlinker')._router('blame', lk)
+      end,
+                lstart = 1,
+      lend = 1,
+              })
           end)
       end)
 end)
