5fc854bb9bc9943c2d89f1d32cf1d7500ba7bad3
linrongbin16 <linrongbin16@outlook.com>
2024-03-08 09:15:13 +0800

    chore

--- spec/gitlinker/linker_spec.lua
+++ spec/gitlinker/linker_spec.lua
@@ -5,13 +5,12 @@ describe("linker", function()
   local assert_true = assert.is_true
   local assert_false = assert.is_false
 
-  before_each(function()
-    vim.api.nvim_command("cd " .. cwd)
-    vim.opt.swapfile = false
-    local gitlinker = require("gitlinker")
-    pcall(gitlinker.setup, {})
-    vim.cmd([[ edit lua/gitlinker.lua ]])
-  end)
+  vim.api.nvim_command("cd " .. cwd)
+  vim.opt.swapfile = false
+  local gitlinker = require("gitlinker")
+  pcall(gitlinker.setup, {})
+  vim.cmd([[ edit lua/gitlinker.lua ]])
+  -- before_each(function() end)
 
   local async = require("gitlinker.commons.async")
   local github_actions = os.getenv("GITHUB_ACTIONS") == "true"
