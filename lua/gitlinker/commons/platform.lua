6fdbffef9d4e6c03718ee646fcf0d9dccfde76e4
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2025-01-18 15:22:41 +0000

    chore(pr): auto-commit

--- lua/gitlinker/commons/platform.lua
+++ lua/gitlinker/commons/platform.lua
@@ -5,7 +5,7 @@ local os_name = uv.os_uname().sysname
 local os_name_valid = type(os_name) == "string" and string.len(os_name) > 0
 
 M.OS_NAME = os_name
-M.IS_WINDOWS = os_name_valid and os_name:gmatch("Windows") ~= nil
+M.IS_WINDOWS = os_name_valid and os_name:match("Windows") ~= nil
 M.IS_MAC = os_name_valid and os_name:match("Darwin") ~= nil
 M.IS_BSD = vim.fn.has("bsd") > 0
 M.IS_LINUX = os_name_valid and os_name:match("Linux") ~= nil
